6606fff60638ebcbc08f6763abbcd5fe1d2afbfd
Alex <Alexander.Schmidt22@web.de>
2023-12-09 01:51:44 +0100

    day4 part 2

--- src/main/java/december4/ScratchCards2.java
+++ src/main/java/december4/ScratchCards2.java
@@ -17,28 +17,18 @@ public class ScratchCards2 {
     private static final Pattern PATTERN_NUMBERS = Pattern.compile("\\d+");
     public static void main(String[] args) {
 
-        String testInput = """
-                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-                Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
-                """;
-
         String content = AdventHelper.readFile("./src/main/java/december4/input.txt");
 
         String[] scratchCardLines = content.split("\\n");
-//        String[] scratchCardLines = testInput.split("\\n");
 
         //Map< cardNumber, List( winningNumbersCount, CopiesOfCardAmount )
         Map<Integer,List<Integer>> cardNumberToWinningNumbersAndCopiesOfCardMap = getNumberOfWinningsForAllScratchCards( scratchCardLines );
 
-        System.out.println("Card : WinningNumberCount before alterations" + cardNumberToWinningNumbersAndCopiesOfCardMap);
+        System.out.println("Card : (WinningNumberCount, Copies) before alterations" + cardNumberToWinningNumbersAndCopiesOfCardMap);
 
         Integer totalNumberOfScratchCards = computeTotalNumberOfScratchCards( cardNumberToWinningNumbersAndCopiesOfCardMap );
 
-        System.out.println("Card : WinningNumberCount after alterations" + cardNumberToWinningNumbersAndCopiesOfCardMap);
+        System.out.println("Card : (WinningNumberCount, Copies) after alterations" + cardNumberToWinningNumbersAndCopiesOfCardMap);
         System.out.println("Total number of scratchcards: " + totalNumberOfScratchCards);
     }
 
@@ -62,7 +52,7 @@ public class ScratchCards2 {
             System.out.println( scratchCardLine );
             System.out.println( actuallyWinningNumbers.size() + " match(es)" );
 
-            cardNumberTowinningNumberCountMap.put( cardNumber, Arrays.asList( actuallyWinningNumbers.size(), /*amountOfCardCopies*/ 1 ) );
+            cardNumberTowinningNumberCountMap.put( cardNumber, Arrays.asList( actuallyWinningNumbers.size(), /*defaultAmountOfCardCopies*/ 1 ) );
 
             cardNumber++;
         }
@@ -81,10 +71,10 @@ public class ScratchCards2 {
 
                 //has to be looped for the amount of winning numbers to increment following cards
                 for (int k = 1; k < winningNumbersCount.get(0) + 1; k++) {
-                    //increment following amount of cards by 1 for each winningNumber in current card
+
+                    //increment following "number of copies" by 1 for each winningNumber in current card
                     if (i + k < map.size() + 1) {
                         map.put( i + k, incrementCardCopiesAmount(map.get(i + k)) );
-//                        System.out.println("current Iteration: Card : WinningNumberCount" + map);
                     }
                 }
             }
@@ -92,33 +82,6 @@ public class ScratchCards2 {
 
         //compute total number of cards from altered Map
         return map.values().stream().map( v -> v.get(1) ).reduce(0, Integer::sum);
-    }
-
-    private static Integer computeTotalNumberOfScratchCards(Map<Integer, List<Integer>> map, int x) {
-        int totalCards = 0;
-
-        for (int i = 1; i <= map.size(); i++) {
-            List<Integer> winningNumbersAndCopiesList = map.get(i);
-
-            int copiesOfCard = winningNumbersAndCopiesList.get(1);
-
-            if (copiesOfCard > 0) {
-                totalCards += copiesOfCard;
-
-                int winningNumbersCount = winningNumbersAndCopiesList.get(0);
-
-                // Increment following amount of cards by 1 for each winningNumber in current card
-                for (int k = 1; k <= winningNumbersCount; k++) {
-                    int nextCardIndex = i + k;
-
-                    if (nextCardIndex <= map.size()) {
-                        map.computeIfPresent(nextCardIndex, (key, value) -> incrementCardCopiesAmount(value));
-                    }
-                }
-            }
-        }
-
-        return totalCards;
     }
 
     //increase number of copies of given card by 1
