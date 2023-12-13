7f7d4bceff52338f4c8c59359ed5a3d3f92f8312
Alex <Alexander.Schmidt22@web.de>
2023-12-14 00:05:59 +0100

    day6 part2 solved

--- src/main/java/december6/BoatRace2.java
+++ src/main/java/december6/BoatRace2.java
@@ -1,4 +1,19 @@
 package december6;
 
+import java.util.stream.LongStream;
+
 public class BoatRace2 {
+    public static void main(String[] args) {
+
+        long time = 62649190;
+        long minDistance = 553101014731074L;
+
+        long numberOfWaysToWinCurrentRace = LongStream
+                .rangeClosed(1, time)
+                .map(x -> x * (time - x))
+                .filter(x -> x > minDistance)
+                .count();
+
+        System.out.println("Number of ways to beat the game: " + numberOfWaysToWinCurrentRace);
+    }
 }
