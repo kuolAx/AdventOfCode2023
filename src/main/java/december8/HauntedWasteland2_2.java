3e8ac079238530d1c681b854654fc8fd3c1631f0
Alex <Alexander.Schmidt22@web.de>
2023-12-20 11:25:12 +0100

    day 8 part 2 wip

--- src/main/java/december8/HauntedWasteland2_2.java
+++ src/main/java/december8/HauntedWasteland2_2.java
@@ -4,10 +4,8 @@ import utils.AdventHelper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,14 +13,8 @@ import java.util.stream.Collectors;
 
 public class HauntedWasteland2_2 {
     private static final Pattern MATCH_WORD_CHARACTERS = Pattern.compile("(\\w+)");
-
-    //Map<String, List<String>> - { NodeName, [L-Instruction, R-Instruction] }
-    private static final Map<String, List<String>> elementMap = new HashMap<>();
-    //char array of "RLLLRLLRRLR...."
-    private static char[] instructions;
-    private static int startingKeysSize;
-    private static long numberOfSteps = 0;
-    private static List<String> startingKeysList;
+    private static final Map<String, List<String>> elementMap = new HashMap<>(); //Map<String, List<String>> - { NodeName, [L-Instruction, R-Instruction] }
+    private static char[] instructions; //char array of "RLLLRLLRRLR...."
     private static String currentKey;
 
     public static void main(String[] args) {
@@ -30,94 +22,56 @@ public class HauntedWasteland2_2 {
         String content = AdventHelper.readFile("./src/main/java/december8/input.txt");
         extractInstructionsAndElementsMap( content );
 
-        System.out.println( "instructions created" );
+        System.out.println( "instructions created - length: " + instructions.length );
         System.out.println( "element-map created" );
 
-        startingKeysList = getStartingNodes();
+        List<String> startingKeysList = getStartingNodes();
         System.out.println("initial Starting Keys: " + startingKeysList);
 
-        startingKeysSize = startingKeysList.size();
+        List<Long> toZStepsList = new ArrayList<>();
 
-        System.out.println("stepsToEndOnZ Lists: ");
-        List<List<Long>> toZStepsLists = new ArrayList<>();
         for (String startNode : startingKeysList) {
 
             String tempKey = startNode;
-            currentKey = tempKey;
-            numberOfSteps = 0;
-            int instructionIndex = 0;
-            List<Long> stepsUntilZList = new ArrayList<>();
+            setCurrentKey( tempKey );
+            long numberOfSteps = 0;
 
-            while ( stepsUntilZList.size() < 10 ) {
+            while ( !getCurrentKey().endsWith("Z") ) {
+                List<Object> results = getNumberOfStepsNeeded(numberOfSteps );
 
-                while ( !currentKey.endsWith("Z") ) {
-                    List<Object> results = getNumberOfStepsNeeded( numberOfSteps, instructionIndex );
-
-                    tempKey             = (String) results.get(0);
-                    numberOfSteps       = (long) results.get(1);
-                    instructionIndex    = (int) results.get(2);
-                }
-
-                if( tempKey.endsWith("Z") ) {
-                    stepsUntilZList.add( numberOfSteps );
-
-                    if ( instructions[instructionIndex] == 'R' ) {
-                        currentKey = elementMap.get( currentKey ).get(1);
-                    } else {
-                        currentKey = elementMap.get( currentKey ).get(0);
-                    }
-                }
+                tempKey             = (String) results.get(0);
+                numberOfSteps       = (long) results.get(1);
             }
 
-            toZStepsLists.add( stepsUntilZList );
-            System.out.println( stepsUntilZList );
+            if( tempKey.endsWith("Z") ) {
+                toZStepsList.add(numberOfSteps);
+            }
+
+            System.out.println( toZStepsList );
         }
 
-        //compare found stepnumbers that end on Z and extract commons, if existent
-//        Set<Long> stepsSet = new HashSet<>( toZStepsLists.get(0) );
-//
-//        for (int i = 1; i < toZStepsLists.size(); i++) {
-//            stepsSet.retainAll( toZStepsLists.get(i) );
-//
-//            if( stepsSet.isEmpty() ) {
-//                System.out.println("no matches found");
-//                break;
-//            } else {
-//                System.out.println("current matches: " + stepsSet);
-//            }
-//        }
+        long lowestCommonMultiple = lcm( toZStepsList );
+        System.out.println("lcm: " + lowestCommonMultiple);
     }
 
-    public static List<Object> getNumberOfStepsNeeded( long numberOfSteps, int instructionIndex ) {
+    public static List<Object> getNumberOfStepsNeeded( long numberOfSteps ) {
 
-        String tempKey = null;
-
-        if( instructionIndex != 0) {
-            System.out.println(currentKey + " " + instructionIndex);
-        }
-
-        for ( int i = instructionIndex; i < instructions.length; i++) {
-            char instruction = instructions[i];
-
-            tempKey = "" + currentKey;
-
-            //never executed??
-            if ( tempKey.endsWith("Z") ) {
-                return List.of( tempKey, numberOfSteps, i );
-            }
+        for (char instruction : instructions) {
 
             //set currentKey to the next value depending on the instruction 'R' or 'L'
             if ( instruction == 'R' ) {
-                currentKey = elementMap.get( currentKey ).get(1);
+                setCurrentKey( elementMap.get( getCurrentKey() ).get(1) );
             } else {
-                currentKey = elementMap.get( currentKey ).get(0);
+                setCurrentKey( elementMap.get( getCurrentKey() ).get(0) );
             }
-
             numberOfSteps++;
+
+            if ( getCurrentKey().endsWith("Z") ) {
+                return List.of( getCurrentKey(), numberOfSteps );
+            }
         }
 
-        tempKey = "" + currentKey;
-        return List.of( tempKey, numberOfSteps, 0 );
+        return List.of( getCurrentKey(), numberOfSteps );
     }
 
     private static List<String> getStartingNodes() {
@@ -144,53 +98,27 @@ public class HauntedWasteland2_2 {
         }
     }
 
-    //my implementation to find the lowest common multiple:
-    private static long getLowestCommonMultiple(List<Long> numbers) {
-
-        int listSize = numbers.size();
-
-        for (int j = 0; j < listSize; j++) {
-
-            Map<Integer, Long> lcms = new HashMap<>();
-
-            for (int i = 1; i < listSize - j; i++) {
-                long a = Math.abs( numbers.get( j ) );
-                long b = Math.abs( numbers.get( j+i ) );
-
-                long lcm = getLowestCommonMultiple( a, b );
-
-                lcms.put( i, lcm );
-                System.out.println(lcms);
-            }
-
-            if ( lcms.size() > 1 )
-                return getLowestCommonMultiple( lcms.values().stream().toList() );
-
-        }
-
-        return 0;
+    //greatestCommonDivisor
+    static long gcd(long a, long b)
+    {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
     }
 
-    private static long getLowestCommonMultiple(long x, long y){
-
-        //greatest common divisor
-        long gcd = getGreatestCommonDivisor( x, y );
-
-        //lowest common multiple = x * y / gcd
-        return x * y / gcd;
+    //lowestCommonMultiple
+    static long lcm(List<Long> numbers)
+    {
+        return numbers.stream().reduce(
+                1L, (x, y) -> (x * y) / gcd(x, y) );
     }
 
-    private static long getGreatestCommonDivisor(long x, long y){
+    public static String getCurrentKey() {
+        return currentKey;
+    }
 
-        if (x == 0 || y == 0) {
-            return x + y;
-        } else {
-            long num1 = Math.abs(x);
-            long num2 = Math.abs(y);
-            long bigNum = Math.max(num1, num2);
-            long smallNum = Math.min(num1, num2);
-            return getGreatestCommonDivisor( bigNum % smallNum, smallNum );
-        }
+    public static void setCurrentKey(String currentKey) {
+        HauntedWasteland2_2.currentKey = currentKey;
     }
 }
 
