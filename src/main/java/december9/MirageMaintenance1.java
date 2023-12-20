af1a82792f720766c21bff9c1ab798517ace0793
Alex <Alexander.Schmidt22@web.de>
2023-12-20 13:21:26 +0100

    day 9 part1 wip

--- src/main/java/december9/MirageMaintenance1.java
+++ src/main/java/december9/MirageMaintenance1.java
@@ -18,14 +18,22 @@ public class MirageMaintenance1 {
             10 13 16 21 30 45""";
     public static void main(String[] args) {
 
-//        String[] contentLines = AdventHelper.readFile("./src/main/java/december9/input.txt").split("\n");
-        String[] testLines = testInput.split("\n");
+        String[] contentLines = AdventHelper.readFile("./src/main/java/december9/input.txt").split("\n");
+//        String[] contentLines = testInput.split("\n");
 
-        List<List<Integer>> sensorHistories = getSensorHistories( testLines );
-
+        List<List<Integer>> sensorHistories = getSensorHistories( contentLines );
         List<Integer> extrapolatedSensorValues = new ArrayList<>();
-        for (List<Integer> sensorHistory : sensorHistories) {
-            extrapolatedSensorValues.add( extrapolateSensorValue( sensorHistory ) );
+
+        for (List<Integer> history : sensorHistories) {
+
+//            history = List.of(12, 12, 11, 5, 5, 5, 40, 194, 558, 1278, 2553, 4643, 7877, 12661, 19486, 28936, 41696, 58560, 80439, 108369, 143519);
+
+            int lastElement = history.get( history.size()-1 );
+            try {
+                extrapolatedSensorValues.add( extrapolateSensorValue( history, lastElement ) );
+            } catch (Exception e) {
+                System.out.println("exception history: " + history);
+            }
         }
 
         System.out.println( extrapolatedSensorValues );
@@ -38,26 +46,26 @@ public class MirageMaintenance1 {
 
         IntStream
                 .range( 0, sensorHistory.size()-1 )
-                .forEach( i -> differences.add( Math.abs(sensorHistory.get(i) - sensorHistory.get(i+1)) ) );
+                .forEach( i -> differences.add( sensorHistory.get(i+1) - sensorHistory.get(i) ) );
 
-        if( differences.stream().distinct().toList().size() > 1 && differences.get(0) != 0)
-            return extrapolateSensorValue( differences, differences.get( differences.size()-1 ) );
+        if( differences.stream().distinct().toList().size() == 1 && differences.get(0) == 0)
+            return lastElement + differences.get( differences.size()-1 );
+        if( differences.size() == 1 && differences.stream().distinct().toList().get(0) != 0 )
+            throw new RuntimeException("list could not be reduced to only zeroes");
 
-        return lastElement + differences.get(0);
-    }
-    private static Integer extrapolateSensorValue(List<Integer> sensorHistory) {
-        return extrapolateSensorValue( sensorHistory, sensorHistory.get( sensorHistory.size()-1 ) );
+        return lastElement + extrapolateSensorValue( differences, differences.get( differences.size()-1 ) );
     }
 
     private static List<List<Integer>> getSensorHistories( String[] contentLines ) {
 
         List<List<Integer>> sensorHistories = new ArrayList<>();
 
-        for (String contentLine : contentLines) {
+        for (int i = 0; i < contentLines.length; i++) {
+            String contentLine = contentLines[i];
             Matcher matchNumbers = MATCH_NUMBERS.matcher(contentLine);
 
             List<Integer> sensorHistory = matchNumbers.results().map(MatchResult::group).map(Integer::valueOf).toList();
-            sensorHistories.add( sensorHistory );
+            sensorHistories.add(sensorHistory);
         }
 
         return sensorHistories;
