0c530dc0193d50e7be040397fb7edd7171f911a2
Alex <Alexander.Schmidt22@web.de>
2023-12-26 13:35:27 +0100

    day 10 wip

--- src/main/java/december10/PipeMaze1.java
+++ src/main/java/december10/PipeMaze1.java
@@ -35,13 +35,52 @@ public class PipeMaze1 {
         NOPIPE, I_VERTICAL, _HORIZONTAL, L_NE, J_NW, SVN_SW, F_SE, START
     }
 
+    public static int lineLength;
+
     public static void main(String[] args) {
 
         String content = AdventHelper.readFile("./src/main/java/december10/input.txt");
         content = testInput1;
-        content = content.replace("\n","");
+        String[] contentLines = content.split("\n");
+        lineLength = contentLines[0].length();
 
-        List<Pipe> path1 = new ArrayList<>();
-        List<Pipe> path2 = new ArrayList<>();
+        int startLine = 0;
+        int startIndex = 0;
+        //get line and index of "S" symbol
+        for (int i = 0; i < contentLines.length; i++) {
+            if (contentLines[i].contains("S")) {
+                startLine = i;
+                startIndex = contentLines[i].indexOf("S");
+            }
+        }
+        System.out.println("start " + startLine + " " + startIndex);
+
+        //scan adjacent positions for connected pipes
+        int previousindex = 0;
+
+        if ( contentLines[startLine].charAt(startIndex+1) == 'J' || contentLines[startLine].charAt(startIndex+1) == '7' || contentLines[startLine].charAt(startIndex+1) == '-'){
+            previousindex = startIndex;
+            startIndex++;
+        } else if ( contentLines[startLine].charAt(startIndex-1) == 'L' || contentLines[startLine].charAt(startIndex-1) == 'F' || contentLines[startLine].charAt(startIndex-1) == '-'){
+            previousindex = startIndex;
+            startIndex--;
+        } else if ( contentLines[startLine-1].charAt(startIndex) == '7' || contentLines[startLine-1].charAt(startIndex) == 'F' || contentLines[startLine-1].charAt(startIndex) == '|' ) {
+            previousindex = startIndex;
+            startLine--;
+        }
+
+        System.out.println("start " + startLine + " " + startIndex);
+
+        //follow path until S is reached again
+        int stepNumber = 1;
+        char currentChar = 0;
+
+        while ( currentChar != 'S' ) {
+
+
+
+        }
+
+
     }
 }
